207066608a28285a006b459e7041362a445901b6
Lee Dano <eksh7080@gmail.com>
2022-11-13 23:57:15 +0900

    Setting Update

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,46 +1,23 @@
-// import logo from './logo.svg';
-// import './App.css';
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
-
-
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import './App.css';
+import React from 'react';
+import { BrowserRouter,  Routes, Route } from 'react-router-dom';
+import  Main from './pages/Main';
+// import axios from 'axios';
 
 function App() {
-    const [hello, setHello] = useState('')
 
-    useEffect(() => {
-        axios.get('/api/test')
-            .then(response => setHello(response.data))
-            .catch(error => console.log(error))
-    }, []);
+    // useEffect(() => {
+    //     axios.get('/api/test')
+    //         .then(response => setHello(response.data))
+    //         .catch(error => console.log(error))
+    // }, []);
 
     return (
-        <div>
-            화면단은 왜 이렇게 다들 어려운거죠 : {hello}
-        </div>
+        <BrowserRouter basename="/">
+                        <Routes>
+                            <Route exact path="/*" element={<Main />} /> {/* => 메인페이지 */}
+                        </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/main/frontend/src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+
+
+    return (
+        <section style={{backgroundColor: "F3B5BB"}}>
+            <p>빵빵</p>
+        </section>
+    )
+}
+
+export default Main;
