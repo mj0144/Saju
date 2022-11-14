ddf6d90acf60bf8357d3efad499b3bf9266df56d
Lee Dano <eksh7080@gmail.com>
2022-11-15 01:02:52 +0900

    Landing Page Update

--- src/main/frontend/src/pages/Main/index.js
+++ src/main/frontend/src/pages/Main/index.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { MainContainer } from "./style";
+import Title from "assets/images/title.png";
+import Visual from "assets/images/visual.png";
+import { MainContainer, Frame } from "./style";
 
 
 const Main = () => {
-
+    const num = 1930;
 
     return (
         <MainContainer>
-        
-            <p>빵빵</p>
+            <Frame>
+                <div>
+                    <h1>우리는 얼마나 잘맞을까?</h1>
+                    <img src={Title} alt="Title" />
+                </div>
+                
+                <img src={Visual} alt="Visual" />
+                <p>현재까지 {num}명이 참여했습니다.</p>
+
+                <article>
+                    <strong>테스트 시작!</strong>
+                </article>
+            </Frame>
         </MainContainer>
     )
 }
--- src/main/frontend/src/pages/Main/style.js
+++ src/main/frontend/src/pages/Main/style.js
@@ -3,6 +3,53 @@ import styled from '@emotion/styled';
 export const MainContainer = styled.section`
     background-color: #F3B5BB;
     width: 100%;
-    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
+export const Frame = styled.div`
+    max-width: 600px;
+    width: 100%;
+    height: 100vh;
+    background-color: #FBE8E7;
+    margin: 6% auto 2%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 30px;
+    text-align: center;
+    
+    & div{
+        &  h1{
+            color: #9B6F73;
+            font-size: 24px;
+            font-weight: 900;
+        }
+    }
+
+    & p{
+        color: #6E6360;
+        font-weight: 900;
+        font-size: 20px;
+    }
+
+    & article{
+        background-color: #EBAAB7;
+        padding: 20px 40px;
+        border-radius: 40px;
+        margin-top: 40px;
+
+        & strong{
+            color: #fff;
+            font-size: 20px;
+            font-weight: 900;
+            cursor: pointer;
+        }
+
+        & strong:hover{
+            color: black;
+        }
+    }
 `
